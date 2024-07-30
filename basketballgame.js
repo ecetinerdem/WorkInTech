6b1f5dea7c717559f04039580ed6e00f5d7c0f45
TheL0nelyG0d <cetoerdem1@gmail.com>
2024-07-30 22:48:32 +0300

    week2 second day

--- basketballgame.js
+++ basketballgame.js
@@ -1,24 +1,26 @@
 const macOzeti = (evSahibiTakim, deplasmanTakimi) => {
-    let evSahibiSkoru = 0;
-    let deplasmanSkoru = 0;
-    let texts = [];
-  
-    const takimSkoru = () => {
-      return Math.floor(Math.random() * 15 + 12);
-    };
-  
-    for (let i = 1; i <= 4; i++) {
-      evSahibiSkoru = evSahibiSkoru + takimSkoru();
-      deplasmanSkoru = deplasmanSkoru + takimSkoru();
-      const text = `${i}. Periyot: ${evSahibiTakim} ${evSahibiSkoru} - ${deplasmanTakimi} ${deplasmanSkoru}`;
-      texts.push(text);
-    }
-    if (evSahibiSkoru === deplasmanSkoru) {
-    } else {
-      const text = `Maç Sonucu: ${evSahibi} ${evSahibiSkoru} - ${deplasmanSkoru} ${
-        evSahibiSkoru > deplasmanSkoru ? 'kazandı' : 'kaybetti'
-      }`;
-      texts.push(text);
-    }
-    return texts;
+  let evSahibiSkoru = 0;
+  let deplasmanSkoru = 0;
+  let texts = [];
+
+  const takimSkoru = () => {
+    return Math.floor(Math.random() * 15 + 12);
   };
+
+  for (let i = 1; i <= 4; i++) {
+    evSahibiSkoru = evSahibiSkoru + takimSkoru();
+    deplasmanSkoru = deplasmanSkoru + takimSkoru();
+    const text = `${i}. Periyot: ${evSahibiTakim} ${evSahibiSkoru} - ${deplasmanTakimi} ${deplasmanSkoru}`;
+    texts.push(text);
+  }
+  if (evSahibiSkoru === deplasmanSkoru) {
+    const text = `Maç ${evSahibiSkoru} - ${deplasmanSkoru} ile uzatmalara gitti`;
+    texts.push(text);
+  } else {
+    const text = `Maç Sonucu: ${evSahibi} ${evSahibiSkoru} - ${deplasmanSkoru} ${
+      evSahibiSkoru > deplasmanSkoru ? 'kazandı' : 'kaybetti'
+    }`;
+    texts.push(text);
+  }
+  return texts;
+};
