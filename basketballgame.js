e79b4f5286fe1732e7ce5c534e4126e5dda8d33c
TheL0nelyG0d <cetoerdem1@gmail.com>
2024-07-30 22:44:28 +0300

    week2 second day

---
+++ basketballgame.js
@@ -0,0 +1,24 @@
+const macOzeti = (evSahibiTakim, deplasmanTakimi) => {
+    let evSahibiSkoru = 0;
+    let deplasmanSkoru = 0;
+    let texts = [];
+  
+    const takimSkoru = () => {
+      return Math.floor(Math.random() * 15 + 12);
+    };
+  
+    for (let i = 1; i <= 4; i++) {
+      evSahibiSkoru = evSahibiSkoru + takimSkoru();
+      deplasmanSkoru = deplasmanSkoru + takimSkoru();
+      const text = `${i}. Periyot: ${evSahibiTakim} ${evSahibiSkoru} - ${deplasmanTakimi} ${deplasmanSkoru}`;
+      texts.push(text);
+    }
+    if (evSahibiSkoru === deplasmanSkoru) {
+    } else {
+      const text = `Maç Sonucu: ${evSahibi} ${evSahibiSkoru} - ${deplasmanSkoru} ${
+        evSahibiSkoru > deplasmanSkoru ? 'kazandı' : 'kaybetti'
+      }`;
+      texts.push(text);
+    }
+    return texts;
+  };
