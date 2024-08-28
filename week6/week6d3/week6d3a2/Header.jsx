c3847067e358dc4b6f8e7ddedf2bd7f75f620efe
ecetinerdem <cetoerdem1@gmail.com>
2024-08-28 18:57:41 +0400

    week6d3a2

---
+++ week6/week6d3/week6d3a2/Header.jsx
@@ -0,0 +1,14 @@
+function Header(props) {
+    const { searchText, handleSearch } = props;
+  
+    return (
+      <>
+        <header>
+          <h1>WiStore Products</h1>
+        </header>
+      </>
+    );
+  }
+  
+  export default Header;
+  
